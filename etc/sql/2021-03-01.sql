44670c38cb8bcb888670973641aa83be12bc7570
JAEHO SONG <thruthesky@gmail.com>
2021-03-02 21:58:41 +0900

    update sql. adding push notification

--- etc/sql/2021-03-01.sql
+++ etc/sql/2021-03-01.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mariadb
--- Generation Time: Mar 01, 2021 at 10:15 AM
+-- Generation Time: Mar 02, 2021 at 12:58 PM
 -- Server version: 10.5.9-MariaDB-1:10.5.9+maria~focal
 -- PHP Version: 8.0.2
 
@@ -39,6 +39,23 @@ CREATE TABLE `wc_categories` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `wc_files`
+--
+
+CREATE TABLE `wc_files` (
+                            `idx` int(10) UNSIGNED NOT NULL,
+                            `userIdx` int(10) UNSIGNED NOT NULL,
+                            `path` varbinary(255) NOT NULL,
+                            `name` varchar(255) NOT NULL,
+                            `type` varchar(32) NOT NULL,
+                            `size` int(10) UNSIGNED NOT NULL,
+                            `createdAt` int(10) UNSIGNED NOT NULL,
+                            `updatedAt` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `wc_metas`
 --
 
@@ -51,6 +68,43 @@ CREATE TABLE `wc_metas` (
                             `createdAt` int(10) UNSIGNED NOT NULL,
                             `updatedAt` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wc_posts`
+--
+
+CREATE TABLE `wc_posts` (
+                            `idx` int(10) UNSIGNED NOT NULL,
+                            `rootIdx` int(10) UNSIGNED NOT NULL DEFAULT 0,
+                            `parentIdx` int(10) UNSIGNED NOT NULL DEFAULT 0,
+                            `categoryIdx` int(10) UNSIGNED NOT NULL,
+                            `userIdx` int(10) UNSIGNED NOT NULL,
+                            `subcategory` varchar(64) NOT NULL DEFAULT '',
+                            `title` varchar(255) NOT NULL DEFAULT '',
+                            `path` varchar(255) NOT NULL DEFAULT '',
+                            `content` longtext NOT NULL DEFAULT '',
+                            `files` text NOT NULL DEFAULT '',
+                            `createdAt` int(10) UNSIGNED NOT NULL,
+                            `updatedAt` int(10) UNSIGNED NOT NULL,
+                            `deletedAt` int(10) UNSIGNED NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wc_push_notification_tokens`
+--
+
+CREATE TABLE `wc_push_notification_tokens` (
+                                               `idx` int(10) UNSIGNED NOT NULL,
+                                               `userIdx` int(10) UNSIGNED NOT NULL,
+                                               `token` varchar(255) NOT NULL,
+                                               `domain` varchar(64) NOT NULL,
+                                               `createdAt` int(10) UNSIGNED NOT NULL,
+                                               `updatedAt` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -83,12 +137,42 @@ ALTER TABLE `wc_categories`
   ADD UNIQUE KEY `id` (`id`);
 
 --
+-- Indexes for table `wc_files`
+--
+ALTER TABLE `wc_files`
+    ADD PRIMARY KEY (`idx`),
+  ADD KEY `userIdx` (`userIdx`);
+
+--
 -- Indexes for table `wc_metas`
 --
 ALTER TABLE `wc_metas`
     ADD PRIMARY KEY (`idx`),
   ADD UNIQUE KEY `taxonomy_entity_code` (`taxonomy`,`entity`,`code`),
   ADD KEY `code` (`code`);
+
+--
+-- Indexes for table `wc_posts`
+--
+ALTER TABLE `wc_posts`
+    ADD PRIMARY KEY (`idx`),
+  ADD KEY `createdAt` (`createdAt`),
+  ADD KEY `updatedAt` (`updatedAt`),
+  ADD KEY `userIdx` (`userIdx`),
+  ADD KEY `categoryIdx` (`categoryIdx`),
+  ADD KEY `subcategory` (`subcategory`),
+  ADD KEY `deletedAt` (`deletedAt`),
+  ADD KEY `parentIdx` (`parentIdx`),
+  ADD KEY `rootIdx` (`rootIdx`),
+  ADD KEY `path` (`path`);
+
+--
+-- Indexes for table `wc_push_notification_tokens`
+--
+ALTER TABLE `wc_push_notification_tokens`
+    ADD PRIMARY KEY (`idx`),
+  ADD UNIQUE KEY `token` (`token`),
+  ADD KEY `domain` (`domain`);
 
 --
 -- Indexes for table `wc_users`
@@ -108,9 +192,27 @@ ALTER TABLE `wc_categories`
     MODIFY `idx` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `wc_files`
+--
+ALTER TABLE `wc_files`
+    MODIFY `idx` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `wc_metas`
 --
 ALTER TABLE `wc_metas`
+    MODIFY `idx` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `wc_posts`
+--
+ALTER TABLE `wc_posts`
+    MODIFY `idx` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `wc_push_notification_tokens`
+--
+ALTER TABLE `wc_push_notification_tokens`
     MODIFY `idx` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
