2c21e32b3a9d425ba2bcd7ac86222b1b596be7f7
JAEHO SONG <thruthesky@gmail.com>
2021-03-03 22:04:27 +0900

    add phpThumb

--- docker/Dockerfile.php
+++ docker/Dockerfile.php
@@ -1,6 +1,6 @@
 FROM php:fpm
 
-RUN docker-php-ext-install pdo pdo_mysql mysqli
+RUN docker-php-ext-install pdo pdo_mysql mysqli gd
 
 # 환경 설정 파일을 실제의 것으로 복사한다.
 RUN mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
