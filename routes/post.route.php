aa3505f5374c027ca1e3fc6f063879e93cac1fd9
JAEHO SONG <thruthesky@gmail.com>
2021-03-11 10:47:19 +0900

    fix. post.gets()

--- routes/post.route.php
+++ routes/post.route.php
@@ -17,18 +17,20 @@ class PostRoute {
         if ( ! isset($in[IDX]) ) return e()->idx_is_empty;
         return post($in[IDX])->get();
     }
-    public function gets($in) {
-    if ( ! isset($in['idxes']) ) return e()->idx_is_empty;
-    $idxes = explode(',', $in['idxes']);
 
-    $rets = [];
-    foreach( $idxes as $idx ) {
-	if ( post($idx)->exists == false ) continue;
-        $post = post($idx)->get();
-	$rets[] = $post;
+    public function gets($in) {
+        if ( ! isset($in['idxes']) ) return e()->idx_is_empty;
+        $idxes = explode(',', $in['idxes']);
+
+        $rets = [];
+        foreach( $idxes as $idx ) {
+            if ( post($idx)->exists() == false ) continue;
+            $post = post($idx)->get();
+            $rets[] = $post;
+        }
+        return $rets;
     }
-    return $rets;
-}
+
     public function search($in) {
         return post()->search(
             where: $in['where'] ?? '1',
