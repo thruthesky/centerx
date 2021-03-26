3fce9e33e738fed47cee6c1596f79a34be32c805
JAEHO SONG <thruthesky@gmail.com>
2021-03-26 16:23:43 +0900

    fix. search key

--- routes/searchKey.route.php
+++ routes/searchKey.route.php
@@ -1,10 +1,22 @@
 <?php
 
 class SearchKeyRoute {
-    public function stats() {
+    public function stats($in) {
         $table = DB_PREFIX . 'search_keys';
-        $q = "SELECT searchKey, COUNT(*) as cnt from $table GROUP BY searchKey ORDER BY cnt DESC";
+
+
+
+        if ( !isset($in['days']) || !$in['days'] ) $in['days'] = 7;
+
+        $stamp_days_ago = time() - $in['days'] * 24 * 60 * 60;
+
+
+
+
+        $q = "SELECT searchKey, COUNT(*) as cnt from $table WHERE createdAt > $stamp_days_ago GROUP BY searchKey ORDER BY cnt DESC, searchKey ASC";
+        debug_log($q);
         $rows = db()->get_results($q, ARRAY_A);
+        $rets = [];
         foreach( $rows as $row ) {
             $rets[$row['searchKey']] = $row['cnt'];
         }
