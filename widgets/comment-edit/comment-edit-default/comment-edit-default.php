3d623e0ae682cb1897e9986fe5ede4632385b976
YinG17 <simbulanja.17@gmail.com>
2021-04-01 10:22:21 +0800

    default comment edit form UI

--- widgets/comment-edit/comment-edit-default/comment-edit-default.php
+++ widgets/comment-edit/comment-edit-default/comment-edit-default.php
@@ -24,20 +24,24 @@ $comment = $o['comment'] ?? null;
     <form enctype="multipart/form-data" action="/" method="POST">
         <input type="hidden" name="p" value="forum.comment.edit.submit">
         <input type="hidden" name="MAX_FILE_SIZE" value="16000000" />
-        <input type="hidden" name="<?=ROOT_IDX?>" value="<?=$post->idx?>">
-        <input type="hidden" name="<?=PARENT_IDX?>" value="<?=$parent->idx?>">
-        <input type="hidden" name="files" id="files<?=$parent->idx?>" value="">
-        <input type="text" name="<?=CONTENT?>">
-        <div>
-            <input name="<?=USERFILE?>" type="file" onchange="onFileChange(event, 'files<?=$parent->idx?>')" />
+        <input type="hidden" name="<?= ROOT_IDX ?>" value="<?= $post->idx ?>">
+        <input type="hidden" name="<?= PARENT_IDX ?>" value="<?= $parent->idx ?>">
+        <input type="hidden" name="files" id="files<?= $parent->idx ?>" value="">
+
+        <div class="d-flex">
+            <div style="width: 95px" class="position-relative overflow-hidden">
+                <button class="btn btn-primary" type="button">Upload</button>
+                <input class="position-absolute top left h-100 opacity-0" name="<?= USERFILE ?>" type="file" onchange="onFileChange(event, 'files<?= $parent->idx ?>')" />
+            </div>
+            <input class="form-control mx-2" type="text" name="<?= CONTENT ?>">
+            <button class="btn btn-primary" type="submit">Submit</button>
         </div>
-        <button type="submit">Submit</button>
     </form>
 </div>
 
 
 <?php
-if ( defined('COMMENT_EDIT_DEFAULT_JAVASCRIPT') ) return;
+if (defined('COMMENT_EDIT_DEFAULT_JAVASCRIPT')) return;
 define('COMMENT_EDIT_DEFAULT_JAVASCRIPT', true);
 ?>
 
@@ -56,30 +60,30 @@ define('COMMENT_EDIT_DEFAULT_JAVASCRIPT', true);
         console.log(event);
         const file = event.target.files[0];
         fileUpload(
-            file,
-            {
-                sessionId: '<?=login()->sessionId?>',
+            file, {
+                sessionId: '<?= login()->sessionId ?>',
             },
-            function (res) {
+            function(res) {
                 console.log("success: res.path: ", res, res.path);
                 const $files = document.getElementById(id);
                 $files.value = addByComma($files.value, res.idx);
             },
             alert,
-            function (p) {
+            function(p) {
                 console.log("pregoress: ", p);
             }
         );
     }
+
     function onClickFileDelete(idx) {
         const re = confirm('Are you sure you want to delete file no. ' + idx + '?');
-        if ( re === false ) return;
+        if (re === false) return;
         axios.post('/index.php', {
-            sessionId: '<?=login()->sessionId?>',
-            route: 'file.delete',
-            idx: idx,
-        })
-            .then(function (res) {
+                sessionId: '<?= login()->sessionId ?>',
+                route: 'file.delete',
+                idx: idx,
+            })
+            .then(function(res) {
                 checkCallback(res, function(res) {
                     console.log('delete success: ', res);
                 }, alert);
