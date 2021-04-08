1d360ede7f273010ef26bcff3ca05daae27f50cd
lancelynyrd <pinedaclp@gmail.com>
2021-04-08 15:13:30 +0800

    layout

--- widgets/post-list/admin-post-list-top-comment/admin-post-list-top-comment.php
+++ widgets/post-list/admin-post-list-top-comment/admin-post-list-top-comment.php
@@ -1,14 +1,14 @@
 <?php
-$total_post = post()->count();
 $now = time();
-$past = $now - 60*60*24*7;
+$past = mktime(0,0,0, date('m',$now), date('d',$now) - 7);
 
-$count_comments = comment()->count(where:"createdAt<=$now AND createdAt>=$past");
+//$top_comments = comment()->search(select: 'userIdx, COUNT(userIdx)' ,where:"createdAt<=$now AND createdAt>=$past GROUP BY userIdx", order: 'COUNT(userIdx)');
+
 
 ?>
 
 <section class="p-4 overflow-hidden" id="admin-post-list-summary" style="height: 23.5rem">
-    <h6 class="text-muted">Total number posts: <?= $count_comments ?></h6>
+<!--    <h6 class="text-muted">Total number posts: --><?//= $count_comments ?><!--</h6>-->
     <h5 class="mb-4">Recent Posts</h5>
 
     <?php foreach (post()->latest(null, 1, 4) as $post) { ?>
