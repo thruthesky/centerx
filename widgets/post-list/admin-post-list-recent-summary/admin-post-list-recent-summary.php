f71572ae2e2d510990843c013441fb13d4cf5f00
YinG17 <simbulanja.17@gmail.com>
2021-04-08 14:01:58 +0800

    recent post section update

--- widgets/post-list/admin-post-list-recent-summary/admin-post-list-recent-summary.php
+++ widgets/post-list/admin-post-list-recent-summary/admin-post-list-recent-summary.php
@@ -6,12 +6,12 @@ $total_post = post()->count();
     <h6 class="text-muted">Total number posts: <?= $total_post ?></h6>
     <h5 class="mb-4">Recent Posts</h5>
 
-    <?php foreach (post()->search() as $post) { ?>
+    <?php foreach (post()->latest(null, 1, 4) as $post) { ?>
         <div class="d-flex mb-3">
             <div class="rounded-circle hw-54x54" style="background-color: grey;">
             </div>
             <div class="text-overflow-ellipsis ml-3">
-                <span><strong><?=$post->title?> asd asd asd sad sadasdsadasd asd </strong></span><br>
+                <span><strong><?=$post->title?></strong></span><br>
                 <span><?=$post->content?></span>
             </div>
         </div>
