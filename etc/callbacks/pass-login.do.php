d052d7f0587cb8e013813050c56694421a9f8c2a
JAEHO SONG <thruthesky@gmail.com>
2021-04-16 12:59:42 +0900

    update on pass login

---
+++ etc/callbacks/pass-login.do.php
@@ -0,0 +1,23 @@
+<?php
+// 인증
+$user = pass_login_callback($_REQUEST);
+if ( isError($user) ) {
+    pass_login_message($user);
+    exit;
+}
+$profile = pass_login_or_register($user);
+if ( isError($profile) ) {
+    //    debug_log("pass-login-callback-php:: error code: $profile");
+    echo "<h1>ERROR: $profile</h1>";
+    exit;
+}
+
+/**
+ * 여기까지 오면 로그인 성공
+ */
+?>
+<?php includeFirebase(); ?>
+<script>
+    const db = firebase.firestore();
+    db.collection('notifications').doc('<?=in('state')?>').set({time: (new Date).getTime(), sessionId: '<?=$profile['sessionId']?>'});
+</script>
