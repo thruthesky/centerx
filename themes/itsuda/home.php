b4b23ec6da441b9eabe01adced091bf5612314f1
JAEHO SONG <thruthesky@gmail.com>
2021-04-22 21:06:12 +0900

    update for doc

--- themes/itsuda/home.php
+++ themes/itsuda/home.php
@@ -2,10 +2,38 @@
 
 
 ?>
+<div class="mb-3 d-flex justify-content-end">
+    <img style="width: 240px;" src="themes/itsuda/img/android-download.png">
+</div>
+
 <img id="home1" class="w-100" src="themes/itsuda/img/home1.jpg">
 <img id="home1" class="w-100" src="themes/itsuda/img/home2.jpg">
 <img id="home3" class="w-100" src="themes/itsuda/img/home3.jpg">
 <img id="home4" class="w-100" src="themes/itsuda/img/home4.jpg">
 <img id="home5" class="w-100" src="themes/itsuda/img/home5.jpg">
 <img id="home6" class="w-100" src="themes/itsuda/img/home6.jpg">
-<img id="home7" class="w-100" src="themes/itsuda/img/home7.jpg">
+<img id="home7" class="w-100" src="themes/itsuda/img/home7.jpg?v=4">
+
+<div class="my-5 p-5 bg-light">
+
+    <div>
+        주식회사 위드플랜잇 | 대표이사 김건호
+    </div>
+    <div>
+        사업자 번호: 683-88-02096 | 통신판매업신고번호: 제 2021-서울강남-02310 호
+    </div>
+    <div>
+        소재지: 서울특별시 강남구 언주로 118 (도곡동, 우성캐릭터199), 7층 1712호
+    </div>
+
+    <div>
+
+        연락처 010-8693-4225 thruthesky@gmail.com
+    </div>
+
+    <hr>
+
+    <div>
+        Copyright(c)2002 by 위드플랜잇
+    </div>
+</div>
