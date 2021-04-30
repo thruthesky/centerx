5bff13e77a914723d28bdb8014358918bf9ca626
YinG17 <simbulanja.17@gmail.com>
2021-04-30 18:59:08 +0800

    UI update

--- widgets/post-list-header/post-list-header-default/post-list-header-default.php
+++ widgets/post-list-header/post-list-header-default/post-list-header-default.php
@@ -13,18 +13,16 @@ $category = $o['category'];
     }
 </style>
 
-<div class="d-flex align-items-center p-2 bg-skyblue white" 
-    style="border-top-left-radius: 8px; 
-    border-top-right-radius: 8px;">
-    <h5 class="mb-0 mr-2"><?= strtoupper($category->id) . ' :' ?></h5>
-    <?php if ($category->exists) { ?>
+<div class="d-flex align-items-center p-2 bg-skyblue white" style="border-radius: 5px">
+    <h5 class="mb-0 mr-2"><?= strtoupper($category->id) ?></h5>
+    <!-- <?php if ($category->exists) { ?>
         <div>
-            <a class="btn btn-sm <?= empty(in('lsub')) ? 'btn-info' : ''?>" href="/?p=forum.post.list&categoryId=<?= in(CATEGORY_ID) ?>">All</a>
+            <a class="btn btn-sm <?= empty(in('lsub')) ? 'btn-info' : '' ?>" href="/?p=forum.post.list&categoryId=<?= in(CATEGORY_ID) ?>">All</a>
             <?php foreach ($category->subcategories as $cat) { ?>
-                <a class="btn btn-sm  <?= in('lsub') == $cat ? 'btn-info' : ''?>" href="/?p=forum.post.list&categoryId=<?= in(CATEGORY_ID) ?>&subcategory=<?= $cat ?>&lsub=<?= $cat ?>"><?= $cat ?></a>
+                <a class="btn btn-sm  <?= in('lsub') == $cat ? 'btn-info' : '' ?>" href="/?p=forum.post.list&categoryId=<?= in(CATEGORY_ID) ?>&subcategory=<?= $cat ?>&lsub=<?= $cat ?>"><?= $cat ?></a>
             <?php } ?>
         </div>
-    <?php } ?>
+    <?php } ?> -->
 
     <span class="flex-grow-1"></span>
     <a class="btn btn-sm btn-success" href="/?p=forum.post.edit<?= inCategoryId() ?><?= inSubcategory() ?><?= inLsub() ?>">
--- widgets/post-list/post-list-default/post-list-default.php
+++ widgets/post-list/post-list-default/post-list-default.php
@@ -10,26 +10,43 @@ $category = $o['category'];
 
 
 ?>
-<section style="padding: 1rem 1rem 0 1rem; background-color: #efefef;">
-    <?php
-    if (!empty($posts)) {
-        foreach ($posts as $post) {
-    ?>
-            <div class="d-flex w-100">
-                <?php include widget('user/user-avatar', ['photoUrl' => $post->user()->shortProfile()['photoUrl'], 'size' => '75']) ?>
-                <a href="<?= $post->url ?><?= lsub(true) ?>" class="w-100" style="text-decoration: none">
-                    <div style="color: black; font-weight: 500; font-size: 1.2rem"><?= $post->title ?></div>
-                    <div style="color: black; font-weight: 500"><?= empty($post->user()->shortProfile()['name']) ? 'No name' : $post->user()->shortProfile()['name'] ?></div>
-                    <div class="text-muted">
-                        <?= $post->subcategory ? "<span class='badge badge-info'> {$post->subcategory} </span>" : "" ?>
-                        <?= ' - ' . 'No. ' . $post->idx ?>
-                        <?= ' - ' . date('r', $post->createdAt) ?>
+<!-- <section style="padding: 1rem 1rem 0 1rem; background-color: #efefef;"> -->
+<div class="d-flex mb-3">
+    <section class="mt-3 flex-grow-1">
+        <?php
+        if (!empty($posts)) {
+            foreach ($posts as $post) {
+        ?> <div class="card border-round p-2 mb-3">
+                    <div class="d-flex w-100">
+                        <?php include widget('user/user-avatar', ['photoUrl' => $post->user()->shortProfile()['photoUrl'], 'size' => '75']) ?>
+                        <a href="<?= $post->url ?><?= lsub(true) ?>" class="w-100" style="text-decoration: none">
+                            <div style="color: black; font-weight: 500; font-size: 1.2rem"><?= $post->title ?></div>
+                            <div style="color: black; font-weight: 500"><?= empty($post->user()->shortProfile()['name']) ? 'No name' : $post->user()->shortProfile()['name'] ?></div>
+                            <div class="text-muted">
+                                <?= $post->subcategory ? "<span class='badge badge-info'> {$post->subcategory} </span>" : "" ?>
+                                <?= ' - ' . 'No. ' . $post->idx ?>
+                                <?= ' - ' . date('r', $post->createdAt) ?>
+                            </div>
+                        </a>
                     </div>
+                </div>
+            <?php }
+        } else { ?>
+            <div class="mt-5 pb-3 d-flex justify-content-center">No posts yet ..</div>
+        <?php } ?>
+    </section>
+
+    <section class="mt-3 ml-3" style="width: 150px;">
+        <div class="card p-2">
+            <?php if ($category->exists) { ?>
+                <a class="btn btn-sm w-100 mb-2 <?= empty(in('lsub')) ? 'btn-info' : '' ?>" href="/?p=forum.post.list&categoryId=<?= in(CATEGORY_ID) ?>">
+                    <?= ek('All Posts', '@T All Posts') ?>
                 </a>
-            </div>
-            <hr>
-        <?php }
-    } else { ?>
-        <div class="pb-3 d-flex justify-content-center">No posts yet ..</div>
-    <?php } ?>
-</section>
+                <hr class="my-1">
+                <?php foreach ($category->subcategories as $cat) { ?>
+                    <a class="btn btn-sm w-100 mb-2 <?= in('lsub') == $cat ? 'btn-info' : '' ?>" href="/?p=forum.post.list&categoryId=<?= in(CATEGORY_ID) ?>&subcategory=<?= $cat ?>&lsub=<?= $cat ?>"><?= $cat ?></a>
+                <?php } ?>
+            <?php } ?>
+        </div>
+    </section>
+</div>
