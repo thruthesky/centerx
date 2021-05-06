c8baa29e3815551d6a5793a8953fcfce6efdb14f
YinG17 <simbulanja.17@gmail.com>
2021-05-06 19:49:49 +0800

    admin forum settings integration test update

--- widgets/category/admin-category-list/sidebar.php
+++ widgets/category/admin-category-list/sidebar.php
@@ -2,7 +2,7 @@
 $category = category(in(ID));
 
 ?>
-<form data-cy="category-settings-form" action="/" method="post">
+<form data-cy="form" action="/" method="post">
     <input type="hidden" name="p" value="admin.index">
     <input type="hidden" name="w" value="category/admin-category-list">
     <input type="hidden" name="mode" value="update">
@@ -26,14 +26,14 @@ $category = category(in(ID));
             <tr>
                 <td><?= ek('Title', '게시판 제목') ?></td>
                 <td>
-                    <input data-cy="category-settings-form-title" class="form-control" name="<?= TITLE ?>" value="<?= $category->title ?>">
+                    <input data-cy="form-title" class="form-control" name="<?= TITLE ?>" value="<?= $category->title ?>">
                 </td>
             </tr>
 
             <tr>
                 <td><?= ek('Description', '설명') ?></td>
                 <td>
-                    <input data-cy="category-settings-form-description" class="form-control" name="<?= DESCRIPTION ?>" value="<?= $category->description ?>">
+                    <input data-cy="form-description" class="form-control" name="<?= DESCRIPTION ?>" value="<?= $category->description ?>">
                 </td>
             </tr>
 
@@ -50,7 +50,7 @@ $category = category(in(ID));
             </tr>
             <tr>
                 <td colspan="2">
-                    <textarea data-cy="category-settings-form-subcategories" class="w-100 form-control" rows="3" name="subcategories"><?= implode(',', $category->subcategories) ?></textarea>
+                    <textarea data-cy="form-subcategories" class="w-100 form-control" rows="3" name="subcategories"><?= implode(',', $category->subcategories) ?></textarea>
                 </td>
             </tr>
 
@@ -69,26 +69,26 @@ $category = category(in(ID));
             <tr>
                 <td><?= ek('Post Create Point', '글 쓰기 포인트') ?></td>
                 <td>
-                    <input data-cy="category-settings-form-post-create-point" class="form-control" type="number" name="<?= Actions::$createPost ?>" value="<?= $category->createPost ?>">
+                    <input data-cy="form-post-create-point" class="form-control" type="number" name="<?= Actions::$createPost ?>" value="<?= $category->createPost ?>">
                 </td>
             </tr>
             <tr>
                 <td><?= ek('Post Delete Point', '글 삭제 포인트') ?></td>
                 <td>
-                    <input data-cy="category-settings-form-post-delete-point" class="form-control" type="number" name="<?= Actions::$deletePost ?>" value="<?= $category->deletePost ?>">
+                    <input data-cy="form-post-delete-point" class="form-control" type="number" name="<?= Actions::$deletePost ?>" value="<?= $category->deletePost ?>">
                 </td>
             </tr>
 
             <tr>
                 <td><?= ek('Comment Create Point', '코멘트 쓰기 포인트') ?></td>
                 <td>
-                    <input data-cy="category-settings-form-comment-create-point" class="form-control" type="number" name="<?= Actions::$createComment ?>" value="<?= $category->createComment ?>">
+                    <input data-cy="form-comment-create-point" class="form-control" type="number" name="<?= Actions::$createComment ?>" value="<?= $category->createComment ?>">
                 </td>
             </tr>
             <tr>
                 <td><?= ek('Comment Delete Point', '코멘트 삭제 포인트') ?></td>
                 <td>
-                    <input data-cy="category-settings-form-comment-delete-point" class="form-control" type="number" name="<?= Actions::$deleteComment ?>" value="<?= $category->deleteComment ?>">
+                    <input data-cy="form-comment-delete-point" class="form-control" type="number" name="<?= Actions::$deleteComment ?>" value="<?= $category->deleteComment ?>">
                 </td>
             </tr>
 
@@ -99,7 +99,6 @@ $category = category(in(ID));
             <tr class="table-light">
                 <td colspan="2">
                     <div class="hint">
-
                         <?= ek(
                             'When the limitation below happens, point will not be increase or decreased. But the user can continue writing posts and comments. And if `Ban on writing` is checked, then the user can no longer be able to write post or comments.',
                             '아래의 제한 설정에 걸리면 포인트 증/감이 발생하지 않습니다. 다만, 글/코멘트는 계속 쓸 수 있는데, 아래의 글/코멘트 제한을 하면, 글/코멘트도 못 쓰게 됩니다.'
@@ -112,15 +111,15 @@ $category = category(in(ID));
             <tr>
                 <td><?= ek('Hour/Count Limit', '시간/수 제한') ?></td>
                 <td class="d-flex align-items-center">
-                    <input class="w-25 form-control" type="number" name="<?= ActivityLimits::$createHourLimit ?>" value="<?= $category->createHourLimit ?>">
+                    <input data-cy="form-create-hour-limit" class="w-25 form-control" type="number" name="<?= ActivityLimits::$createHourLimit ?>" value="<?= $category->createHourLimit ?>">
                     <span class="mx-2">/</span>
-                    <input class="w-25 form-control" type="number" name="<?= ActivityLimits::$createHourLimitCount ?>" value="<?= $category->createHourLimitCount ?>">
+                    <input data-cy="form-create-hour-limit-count" class="w-25 form-control" type="number" name="<?= ActivityLimits::$createHourLimitCount ?>" value="<?= $category->createHourLimitCount ?>">
                 </td>
             </tr>
             <tr>
                 <td><?= ek('Day/Count Limit', '일/수 제한') ?></td>
                 <td>
-                    <input class="w-25 form-control" type="number" name="<?= ActivityLimits::$createDailyLimitCount ?>" value="<?= $category->createDailyLimitCount ?>">
+                    <input data-cy="form-create-daily-limit-count" class="w-25 form-control" type="number" name="<?= ActivityLimits::$createDailyLimitCount ?>" value="<?= $category->createDailyLimitCount ?>">
                 </td>
             </tr>
 
@@ -128,11 +127,11 @@ $category = category(in(ID));
                 <td><?= ek('Ban on wriiting', '글/코멘트에 제한') ?></td>
                 <td>
                     <label>
-                        <input type="radio" name="<?= ActivityLimits::$banCreateOnLimit ?>" value="Y" <?php if ($category->banCreateOnLimit == 'Y') echo 'checked' ?>> <?= ek('Yes', '예') ?>,
+                        <input data-cy="form-ban-create-on-limit-Y" type="radio" name="<?= ActivityLimits::$banCreateOnLimit ?>" value="Y" <?php if ($category->banCreateOnLimit == 'Y') echo 'checked' ?>> <?= ek('Yes', '예') ?>,
                     </label>
                     &nbsp;
                     <label>
-                        <input type="radio" name="<?= ActivityLimits::$banCreateOnLimit ?>" value="N" <?php if ($category->banCreateOnLimit != 'Y') echo 'checked' ?>> <?= ek('No', '아니오') ?>
+                        <input data-cy="form-ban-create-on-limit-N" type="radio" name="<?= ActivityLimits::$banCreateOnLimit ?>" value="N" <?php if ($category->banCreateOnLimit != 'Y') echo 'checked' ?>> <?= ek('No', '아니오') ?>
                     </label>
                 </td>
             </tr>
@@ -157,7 +156,7 @@ $category = category(in(ID));
             <tr>
                 <td><?= ek('Post create limit', '글 쓰기 허용 포인트 보유량') ?></td>
                 <td>
-                    <input class="form-control" name="postCreateLimit" value="<?= $category->postCreateLimit ?>">
+                    <input data-cy="form-post-create-limit" class="form-control" name="postCreateLimit" value="<?= $category->postCreateLimit ?>">
                 </td>
             </tr>
 
@@ -165,7 +164,7 @@ $category = category(in(ID));
             <tr>
                 <td><?= ek('Comment creation limit', '코멘트 쓰기 허용 포인트 보유량') ?></td>
                 <td>
-                    <input class="form-control" name="commentCreateLimit" value="<?= $category->commentCreateLimit ?>">
+                    <input data-cy="form-comment-create-limit" class="form-control" name="commentCreateLimit" value="<?= $category->commentCreateLimit ?>">
                 </td>
             </tr>
 
@@ -173,7 +172,7 @@ $category = category(in(ID));
             <tr>
                 <td><?= ek('Post/comment read limit', '글/코멘트 읽기 허용 포인트 보유량') ?></td>
                 <td>
-                    <input class="form-control" name="readLimit" value="<?= $category->readLimit ?>">
+                    <input data-cy="form-read-limit" class="form-control" name="readLimit" value="<?= $category->readLimit ?>">
                 </td>
             </tr>
 
@@ -189,11 +188,11 @@ $category = category(in(ID));
                 <td><?= ek('Return To After Edit', '글 편집 후 이동') ?></td>
                 <td>
                     <label>
-                        <input type="radio" name="returnToAfterPostEdit" value="V" <?php if ($category->returnToAfterPostEdit == 'V' || empty($category->returnToAfterPostEdit)) echo 'checked' ?>> <?= ek('Post view page', '글 읽기 페이지') ?>,
+                        <input data-cy="form-return-to-after-post-edit-V" type="radio" name="returnToAfterPostEdit" value="V" <?php if ($category->returnToAfterPostEdit == 'V' || empty($category->returnToAfterPostEdit)) echo 'checked' ?>> <?= ek('Post view page', '글 읽기 페이지') ?>,
                     </label>
                     &nbsp;
                     <label>
-                        <input type="radio" name="returnToAfterPostEdit" value="L" <?php if ($category->returnToAfterPostEdit == 'L') echo 'checked' ?>> <?= ek('Post list page', '글 목록 페이지') ?>
+                        <input data-cy="form-return-to-after-post-edit-L" type="radio" name="returnToAfterPostEdit" value="L" <?php if ($category->returnToAfterPostEdit == 'L') echo 'checked' ?>> <?= ek('Post list page', '글 목록 페이지') ?>
                     </label>
                 </td>
             </tr>
@@ -269,24 +268,24 @@ $category = category(in(ID));
                 <td><?= ek('Post list under view page', '글 읽기 아래 목록') ?></td>
                 <td>
                     <label>
-                        <input type="radio" name="listOnView" value="Y" <?php if ($category->listOnView == 'Y') echo 'checked' ?>> <?= ek('Yes', '예') ?>,
+                        <input data-cy="form-list-on-view-Y" type="radio" name="listOnView" value="Y" <?php if ($category->listOnView == 'Y') echo 'checked' ?>> <?= ek('Yes', '예') ?>,
                     </label>
                     &nbsp;
                     <label>
-                        <input type="radio" name="listOnView" value="N" <?php if ($category->listOnView != 'Y') echo 'checked' ?>> <?= ek('No', '아니오') ?>
+                        <input data-cy="form-list-on-view-N" type="radio" name="listOnView" value="N" <?php if ($category->listOnView != 'Y') echo 'checked' ?>> <?= ek('No', '아니오') ?>
                     </label>
                 </td>
             </tr>
             <tr>
                 <td><?= ek('No of posts per page', '페이지 글 수') ?></td>
                 <td>
-                    <input class="form-control" name="noOfPostsPerPage" type="text" value="<?= $category->noOfPostsPerPage ?>">
+                    <input data-cy="form-post-per-page" class="form-control" name="noOfPostsPerPage" type="text" value="<?= $category->noOfPostsPerPage ?>">
                 </td>
             </tr>
             <tr>
                 <td nowrap><?= ek('No of pages on navigator', '네이게이션 페이지 수') ?></td>
                 <td>
-                    <input class="form-control" name="noOfPagesOnNav" type="text" value="<?= $category->noOfPagesOnNav ?>">
+                    <input data-cy="form-pages-on-nav" class="form-control" name="noOfPagesOnNav" type="text" value="<?= $category->noOfPagesOnNav ?>">
                 </td>
             </tr>
 
@@ -299,14 +298,14 @@ $category = category(in(ID));
                 <td><?= ek('Post List Widget', '글 목록 위젯') ?></td>
                 <td>
                     <label class="">
-                        <input type="radio" name="mobilePostListWidget" value="text" <?php if ($category->mobilePostListWidget == 'text') echo 'checked' ?>> <?= ek('Text', '텍스트') ?>
+                        <input data-cy="form-mobile-post-list-text" type="radio" name="mobilePostListWidget" value="text" <?php if ($category->mobilePostListWidget == 'text') echo 'checked' ?>> <?= ek('Text', '텍스트') ?>
                     </label>
                     <label class="ps-2">
-                        <input type="radio" name="mobilePostListWidget" value="gallery" <?php if ($category->mobilePostListWidget == 'gallery') echo 'checked' ?>> <?= ek('Gallery', '갤러리') ?>
+                        <input data-cy="form-mobile-post-list-gallery" type="radio" name="mobilePostListWidget" value="gallery" <?php if ($category->mobilePostListWidget == 'gallery') echo 'checked' ?>> <?= ek('Gallery', '갤러리') ?>
                     </label>
 
                     <label class="ps-2">
-                        <input type="radio" name="mobilePostListWidget" value="thumbnail" <?php if ($category->mobilePostListWidget == 'thumbnail') echo 'checked' ?>> <?= ek('Thumbnail', '썸네일') ?>
+                        <input data-cy="form-mobile-post-list-thumbnail" type="radio" name="mobilePostListWidget" value="thumbnail" <?php if ($category->mobilePostListWidget == 'thumbnail') echo 'checked' ?>> <?= ek('Thumbnail', '썸네일') ?>
                     </label>
 
                 </td>
@@ -316,10 +315,10 @@ $category = category(in(ID));
                 <td><?= ln(['en' => 'Post View Widget', 'ko' => '글 읽기 위젯']) ?></td>
                 <td>
                     <label class="">
-                        <input type="radio" name="mobilePostViewWidget" value="default" <?php if ($category->mobilePostViewWidget == 'default') echo 'checked' ?>> <?= ek('Default', '기본') ?>
+                        <input data-cy="form-mobile-post-view-default" type="radio" name="mobilePostViewWidget" value="default" <?php if ($category->mobilePostViewWidget == 'default') echo 'checked' ?>> <?= ek('Default', '기본') ?>
                     </label>
                     <label class="ps-2">
-                        <input type="radio" name="mobilePostViewWidget" value="slide" <?php if ($category->mobilePostViewWidget == 'slide') echo 'checked' ?>> <?= ek('Slide', '슬라이드') ?>
+                        <input data-cy="form-mobile-post-view-slide" type="radio" name="mobilePostViewWidget" value="slide" <?php if ($category->mobilePostViewWidget == 'slide') echo 'checked' ?>> <?= ek('Slide', '슬라이드') ?>
                     </label>
 
                 </td>
@@ -327,7 +326,7 @@ $category = category(in(ID));
 
             <tr>
                 <td colspan="2">
-                    <button class="btn btn-sm btn-success w-100" data-cy="category-settings-form-submit" type="submit">Submit</button>
+                    <button class="btn btn-sm btn-success w-100" data-cy="form-submit" type="submit">Submit</button>
                 </td>
             </tr>
         </tbody>
