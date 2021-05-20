2c003a89e250b4eea61636c6f8ad5bde691322c2
YinG17 <simbulanja.17@gmail.com>
2021-05-20 18:31:42 +0800

    update for UI widgets

--- widgets/post/four-stories-with-thumbnail/four-stories-with-thumbnail.php
+++ widgets/post/four-stories-with-thumbnail/four-stories-with-thumbnail.php
@@ -27,7 +27,7 @@ $posts = array_merge($posts, postMockData($lack, photo: true));
     <?php
     foreach ($posts as $post) { ?>
       <div class="story">
-        <?php include widget('post/photo-with-text-at-bottom', ['post' => $post ]); ?>
+        <?php include widget('post/photo-with-text-at-bottom', ['post' => $post, 'imageHeight' => $op['imageHeight'] ?? 150, 'imageWidth' => $op['imageWidth'] ?? 100 ]); ?>
       </div>
     <?php } ?>
   </div>
