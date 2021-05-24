64db44eea00d87e6a76d9e797ad18c8a413d5427
YinG17 <simbulanja.17@gmail.com>
2021-05-24 14:42:04 +0800

    forum UI update

--- widgets/post-list/text-with-thumbnail/text-with-thumbnail.php
+++ widgets/post-list/text-with-thumbnail/text-with-thumbnail.php
@@ -8,16 +8,17 @@ $posts = $o['posts'];
 ?>
 
 <section class="post-list-text-with-thumbnail px-2 px-lg-0">
-  <div class="p-3" style="background-color: #efefef;">
-    <?php
-    if (!empty($posts)) {
-      foreach ($posts as $post) {
-        $post = post(idx: $post->idx);
-        include widget('post/thumbnail-with-title-and-content', ['post' => $post]);
-      }
-    } else {
+  <?php
+  if (!empty($posts)) {
+    foreach ($posts as $post) {
+      $post = post(idx: $post->idx);
+  ?>
+      <div class="p-3 mt-2 rounded" style="background-color: #efefef;">
+        <?php include widget('post/thumbnail-with-title-and-content', ['post' => $post]); ?>
+      </div>
+    <?php }
+  } else {
     ?>
-      <?php include widget('post-list/empty-post-list'); ?>
-    <?php } ?>
-  </div>
+    <?php include widget('post-list/empty-post-list'); ?>
+  <?php } ?>
 </section>
