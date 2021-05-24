6e7192805327aa76886c99371dbc154af00a8d71
YinG17 <simbulanja.17@gmail.com>
2021-05-24 20:45:50 +0800

    forum UI and widgets update

--- widgets/post-view/post-view-default/post-view-default.php
+++ widgets/post-view/post-view-default/post-view-default.php
@@ -22,36 +22,8 @@ $comments = $post->comments();
             <!-- FILES -->
             <?php include widget('files-display/files-display-default', ['files' => $post->files()]) ?>
             <hr class="my-1">
-            <div class="d-flex align-items-center buttons mt-2">
-                <div class="d-flex">
-                    <vote-buttons
-                            parent-idx="<?= $post->idx ?>" y="<?= $post->Y ?>" n="<?= $post->N ?>"
-                            text-like="<?=ln('like')?>"
-                            text-dislike="<?=ln('dislike')?>"
-                    ></vote-buttons>
-                    <?php if ( $post->isMine() == false ) {?><a class="btn btn-sm mr-2" href="<?=messageSendUrl($post->userIdx)?>"><?=ln('send_message')?></a><?php } ?>
-                </div>
-                <span class="flex-grow-1"></span>
-                <a class="btn btn-sm mr-1" href="/?p=forum.post.list&categoryId=<?= $post->categoryId() ?>"><?=ln('list')?></a>
-
-                <?php if ($post->isMine() || admin()) { ?>
-                    <b-dropdown size="lg"  variant="link" toggle-class="text-decoration-none" no-caret>
-                        <template #button-content>
-                            <i class="fa fa-ellipsis-h dark fs-md"></i><span class="sr-only">Search</span>
-                        </template>
-                        <b-dropdown-item href="<?=postEditUrl(postIdx: $post->idx)?>"><?=ln('edit')?></b-dropdown-item>
-                        <b-dropdown-item href="<?=postDeleteUrl($post->idx)?>"  onclick="return confirm('<?=ln('confirm_delete')?>')"><div class="red"><?=ln('delete')?></div></b-dropdown-item>
-                        <?php if (admin()) { ?>
-                            <b-dropdown-divider></b-dropdown-divider>
-                        <b-dropdown-group id="dropdown-group-1" header="Admin">
-                        <b-dropdown-item href="<?=postMessagingUrl($post->idx)?>"><?=ln('admin push')?></b-dropdown-item>
-                        </b-dropdown-group>
-                        <?php } ?>
-                    </b-dropdown>
-                <?php } ?>
-
-
-            </div>
+            <!-- BUTTONS -->
+            <?php include widget('post-buttons/post-buttons-default', ['post' => $post]); ?>
         </section>
         <div class="pt-2">
             <!--
