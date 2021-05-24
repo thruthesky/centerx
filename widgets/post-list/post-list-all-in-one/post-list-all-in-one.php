23801576a2a0c31e0c9453a5d64614e6f010c62b
YinG17 <simbulanja.17@gmail.com>
2021-05-24 20:37:41 +0800

    all in one widget UI update

--- widgets/post-list/post-list-all-in-one/post-list-all-in-one.php
+++ widgets/post-list/post-list-all-in-one/post-list-all-in-one.php
@@ -36,27 +36,44 @@ $posts = $o['posts'];
                 <div class="content p-2 bg-white">
                     <?= nl2br($post->content) ?>
                 </div>
+                <div class="d-flex align-items-center buttons mt-2">
+                    <div class="d-flex">
+                        <vote-buttons parent-idx="<?= $post->idx ?>" y="<?= $post->Y ?>" n="<?= $post->N ?>" text-like="<?= ln('like') ?>" text-dislike="<?= ln('dislike') ?>"></vote-buttons>
+                        <?php if ($post->isMine() == false) { ?><a class="btn btn-sm mr-2" href="<?= messageSendUrl($post->userIdx) ?>"><?= ln('send_message') ?></a><?php } ?>
+                    </div>
+                    <span class="flex-grow-1"></span>
+                    <a class="btn btn-sm mr-1" href="/?p=forum.post.list&categoryId=<?= $post->categoryId() ?>"><?= ln('list') ?></a>
+
+                    <?php if ($post->isMine() || admin()) { ?>
+                        <b-dropdown size="lg" variant="link" toggle-class="text-decoration-none" no-caret>
+                            <template #button-content>
+                                <i class="fa fa-ellipsis-h dark fs-md"></i><span class="sr-only">Search</span>
+                            </template>
+                            <b-dropdown-item href="<?= postEditUrl(postIdx: $post->idx) ?>"><?= ln('edit') ?></b-dropdown-item>
+                            <b-dropdown-item href="<?= postDeleteUrl($post->idx) ?>" onclick="return confirm('<?= ln('confirm_delete') ?>')">
+                                <div class="red"><?= ln('delete') ?></div>
+                            </b-dropdown-item>
+                            <?php if (admin()) { ?>
+                                <b-dropdown-divider></b-dropdown-divider>
+                                <b-dropdown-group id="dropdown-group-1" header="Admin">
+                                    <b-dropdown-item href="<?= postMessagingUrl($post->idx) ?>"><?= ln('admin push') ?></b-dropdown-item>
+                                </b-dropdown-group>
+                            <?php } ?>
+                        </b-dropdown>
+                    <?php } ?>
+                </div>
                 <div class="files">
                     <?php include widget('files-display/files-display-default', ['files' => $post->files()]) ?>
                 </div>
                 <div class="mt-3">
-                    <comment-form 
-                        root-idx="<?= $post->idx ?>" 
-                        parent-idx="<?= $post->idx ?>" 
-                        text-submit="<?= ln('submit') ?>" 
-                        text-cancel="<?= ln('cancel') ?>">
+                    <comment-form root-idx="<?= $post->idx ?>" parent-idx="<?= $post->idx ?>" text-submit="<?= ln('submit') ?>" text-cancel="<?= ln('cancel') ?>">
                     </comment-form>
                 </div>
                 <div class="comments mt-3">
                     <?php foreach ($post->comments() as $comment) { ?>
                         <div class="mb-2 p-1 rounded" style="margin-left: <?= $comment->depth * 16 ?>px; background-color: #e0e0e0;">
                             <?php include widget('comment-view/comment-view-default', ['post' => $post, 'comment' => $comment]); ?>
-                            <comment-form 
-                                root-idx="<?= $post->idx ?>" 
-                                parent-idx="<?= $comment->idx ?>" 
-                                text-submit="<?= ln('submit') ?>" 
-                                text-cancel="<?= ln('cancel') ?>" 
-                                v-if="displayCommentForm[<?= $comment->idx ?>] === 'reply'">
+                            <comment-form root-idx="<?= $post->idx ?>" parent-idx="<?= $comment->idx ?>" text-submit="<?= ln('submit') ?>" text-cancel="<?= ln('cancel') ?>" v-if="displayCommentForm[<?= $comment->idx ?>] === 'reply'">
                             </comment-form>
                         </div>
                     <?php } ?>
