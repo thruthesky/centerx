d7389e84141143c80df514d392b0478daa51eae7
JaeHo Song <thruthesky@gmail.com>
2021-05-26 12:18:45 +0900

    update for story group

--- widgets/post/two-column-story-group-a/two-column-story-group-a.php
+++ widgets/post/two-column-story-group-a/two-column-story-group-a.php
@@ -17,14 +17,14 @@ $reminderPost = $o['reminderPost'] ?? null;
 ?>
 
 <div class="two-column-story-group-a row">
-    <div class="column-a col-12 col-xl-7">
+    <div class="column-a col-12 col-lg-7">
         <?php include widget('post/one-left-photo-with-stories', ['post' => $firstStoriesCategory]) ?>
         <?php include widget('post/reminder', ['post' => $reminderPost]) ?>
         <div class="mt-3">
             <?php include widget('post/four-photo-with-title', ['categoryId' => $secondStoriesCategory]) ?>
         </div>
     </div>
-    <div class="column-b mt-3 col-12 col-xl-5">
+    <div class="column-b mt-3 col-12 col-lg-5">
         <?php include widget('post/2x2-photo-top-text-bottom', ['categoryId' => $thirdStoriesCategory, 'imageHeight' => 75]) ?>
         <div class="bottom-stories bg-light p-3 border-radius-md">
             <?php include widget('post/story-list-with-bullet', ['categoryId' => $fourthStoriesCategory, 'limit' => 5]) ?>
