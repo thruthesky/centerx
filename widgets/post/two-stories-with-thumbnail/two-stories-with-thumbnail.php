ec6ca6c982a0cc5ace58ccbe8bfc3cc822860d48
YinG17 <simbulanja.17@gmail.com>
2021-05-26 14:25:10 +0800

    widget renaming, remove unused css codes

--- widgets/post/two-stories-with-thumbnail/two-stories-with-thumbnail.php
+++ widgets/post/two-stories-with-thumbnail/two-stories-with-thumbnail.php
@@ -2,21 +2,22 @@
 
 /**
  * @size wide
- * @options 'categoryId'
+ * @options 'categoryId', 'limit'
  * @dependency none
  * @description displays 2 stacked post, each post contains a thumbnail with title and content. It use's 'thumbnail-left-title-and-content-right' as child widget.
  */
 $op = getWidgetOptions();
 
-$categoryId = 'discussion';
+$limit = $op['limit'] ?? 2;
+
 $posts = [];
 if (isset($op['categoryId'])) {
-  $posts = post()->latest(categoryId: $op['categoryId'], limit: 2);
+  $posts = post()->latest(categoryId: $op['categoryId'], limit: $limit);
 }
 
-if (empty($posts)) {
-  $posts = postMockData(2, photo: true);
-}
+$lack = $limit - count($posts);
+$posts = array_merge($posts, postMockData($lack, photo: true));
+
 ?>
 
 <div class="two-stories-with-thumbnail">
