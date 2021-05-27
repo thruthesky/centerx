f5ef94707c5dc19251eac4f027a28f5baf5de5a9
YinG17 <simbulanja.17@gmail.com>
2021-05-27 20:34:25 +0800

    update for responsive post list header UI

--- widgets/post-list-header/post-list-header-default/post-list-header-default.php
+++ widgets/post-list-header/post-list-header-default/post-list-header-default.php
@@ -13,17 +13,17 @@ $category = $o['category'];
     <div class="d-flex justify-content-end flex-wrap">
         <?php if ($category->exists) { ?>
             <div class="px-2">
-                <span class="mr-2 text-uppercase font-weight-bold"><?= $category->id ?></span>
+                <span class="m-1 text-uppercase font-weight-bold"><?= $category->id ?></span>
                 <?php if ($category->subcategories) { ?>
-                    <a class="mr-2 mb-2 btn btn-sm btn-info" href="/?p=forum.post.list&categoryId=<?= in(CATEGORY_ID) ?>">All</a>
+                    <a class="m-1 btn btn-sm btn-info" href="/?p=forum.post.list&categoryId=<?= in(CATEGORY_ID) ?>">All</a>
                     <?php foreach ($category->subcategories as $cat) { ?>
-                        <a class="mr-2 mb-2 btn btn-sm btn-info" href="/?p=forum.post.list&categoryId=<?= in(CATEGORY_ID) ?>&subcategory=<?= $cat ?>"><?= $cat ?></a>
+                        <a class="m-1 btn btn-sm btn-info" href="/?p=forum.post.list&categoryId=<?= in(CATEGORY_ID) ?>&subcategory=<?= $cat ?>"><?= $cat ?></a>
                     <?php } ?>
                 <?php } ?>
             </div>
         <?php } ?>
         <span class="d-md-block flex-grow-1"></span>
-        <div class="d-flex align-items-center mr-2">
+        <div class="d-flex align-items-center mr-2 p-1">
             <span class="mr-2">
                 <?php include widget('push-notification/push-notification-icon', ['topic' => NOTIFY_POST . $category->id, 'label' => ln('post')]) ?>
             </span>
