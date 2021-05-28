c7014b49f4d8cbd74b31cf72674a414cb25cf272
YinG17 <simbulanja.17@gmail.com>
2021-05-28 14:14:27 +0800

    added user menu popover

--- widgets/post-meta/post-meta-default/post-meta-default.php
+++ widgets/post-meta/post-meta-default/post-meta-default.php
@@ -6,13 +6,18 @@ $o = getWidgetOptions();
  */
 $post = $o['post'];
 
+$avatarPopoverId = "user-avatar-" . $post->idx;
+$usernamePopoverId = "user-name-" . $post->idx;
 ?>
 
-
 <div class="post-meta-default d-flex">
-    <?php include widget('user/user-avatar', ['user' => $post->user(), 'size' => '70']) ?>
+    <div id="<?= $avatarPopoverId ?>" class="pointer" @click="openPopover('<?= $avatarPopoverId ?>')" tabindex="0">
+        <?php include widget('user/user-avatar', ['user' => $post->user(), 'size' => '70']) ?>
+    </div>
     <div class="meta">
-        <div><b><?= $post->user()->nicknameOrName ?></b></div>
+        <div>
+            <b id="<?= $usernamePopoverId ?>" class="pointer block" @click="openPopover('<?= $usernamePopoverId ?>')" tabindex="0"><?= $post->user()->nicknameOrName ?></b>
+        </div>
         <div class="text-muted">
             <?= $post->subcategory ? "<span class='badge badge-info'>{$post->subcategory}</span> " : "" ?>
             <?php if ($_ = hook()->run('post-meta-3rd-line', $post)) echo $_;
@@ -21,8 +26,13 @@ $post = $o['post'];
             <?php } ?>
         </div>
         <div class="text-muted">
-            <?= ln('date') ?>: <?= $post->shortDate ?> • 
+            <?= ln('date') ?>: <?= $post->shortDate ?> •
             <?= ln('no_of_views') ?>: <?= $post->noOfViews ?>
         </div>
     </div>
 </div>
+
+<forum-popup-menu :id="'<?= $avatarPopoverId ?>'"></forum-popup-menu>
+<forum-popup-menu :id="'<?= $usernamePopoverId ?>'"></forum-popup-menu>
+
+<?php js(HOME_URL . 'etc/js/vue-js-components/user-popup-menu.js') ?>
