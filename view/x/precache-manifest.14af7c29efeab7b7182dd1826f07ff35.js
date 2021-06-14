a689537adde21abcf7ee8613773a07ccbfbeac6d
Selrahc <pinedaclp@gmail.com>
2021-06-14 16:06:14 +0800

    build sample

---
+++ view/x/precache-manifest.14af7c29efeab7b7182dd1826f07ff35.js
@@ -0,0 +1,286 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b03bda5b5a8c03dd34a6",
+    "url": "/css/app.55a27b44.css"
+  },
+  {
+    "revision": "b03bda5b5a8c03dd34a6",
+    "url": "/css/app.55a27b44.css.map"
+  },
+  {
+    "revision": "82c8ed2914602e218fdb",
+    "url": "/css/chunk-vendors.55204a1e.css"
+  },
+  {
+    "revision": "82c8ed2914602e218fdb",
+    "url": "/css/chunk-vendors.55204a1e.css.map"
+  },
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "5713ba23956bf12d2de3c04d4d58365b",
+    "url": "/firebase-messaging-sw.js"
+  },
+  {
+    "revision": "cae98c5774a2175c5103fd632b57d9e7",
+    "url": "/img/cafe/root-domain-logo/no-logo.jpg"
+  },
+  {
+    "revision": "63bc2beff6d55e6ac8d6a0dc05290b1c",
+    "url": "/img/cafe/root-domain-logo/philov-logo.jpg"
+  },
+  {
+    "revision": "f2c26d4b469abd4b8714260515813ae8",
+    "url": "/img/camera.f2c26d4b.svg"
+  },
+  {
+    "revision": "1f0cd80f963843f6307a56b36aca813f",
+    "url": "/img/ellipsis-v.1f0cd80f.svg"
+  },
+  {
+    "revision": "4ec22c43bad73cb23c0d2d6992414a16",
+    "url": "/img/external-link.4ec22c43.svg"
+  },
+  {
+    "revision": "7677c3c9b878d83784fd86fafcb09ca8",
+    "url": "/img/fas-trash.7677c3c9.svg"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "845a39478d0e2d4d5d32a092de2de250",
+    "url": "/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "2695f5feb66cdb0c6f09d0e415824cf9",
+    "url": "/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "6de16254d1b4193353e7015f9177535d",
+    "url": "/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "b2df8abced56e0bbd49f7878a411e9c0",
+    "url": "/img/kakao.b2df8abc.png"
+  },
+  {
+    "revision": "d7836f6e0124ffb64f3dbb3b7dfa6fd2",
+    "url": "/img/naver.d7836f6e.png"
+  },
+  {
+    "revision": "c9055354cc27565aa7e9908faae75550",
+    "url": "/img/search.c9055354.svg"
+  },
+  {
+    "revision": "db962f69fc7a6a0efa61f0f774ad98a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81062d60e16525697a5a",
+    "url": "/js/about.91928a22.js"
+  },
+  {
+    "revision": "81062d60e16525697a5a",
+    "url": "/js/about.91928a22.js.map"
+  },
+  {
+    "revision": "b03bda5b5a8c03dd34a6",
+    "url": "/js/app.d9a55026.js"
+  },
+  {
+    "revision": "b03bda5b5a8c03dd34a6",
+    "url": "/js/app.d9a55026.js.map"
+  },
+  {
+    "revision": "f7d067919ec60c149e9d",
+    "url": "/js/chunk-1873a01f.76ec4f54.js"
+  },
+  {
+    "revision": "f7d067919ec60c149e9d",
+    "url": "/js/chunk-1873a01f.76ec4f54.js.map"
+  },
+  {
+    "revision": "84c8d488199027844d88",
+    "url": "/js/chunk-2d0aa5d2.8741daa0.js"
+  },
+  {
+    "revision": "84c8d488199027844d88",
+    "url": "/js/chunk-2d0aa5d2.8741daa0.js.map"
+  },
+  {
+    "revision": "3e3171a4539ee56e0495",
+    "url": "/js/chunk-2d0c1282.32ef39aa.js"
+  },
+  {
+    "revision": "3e3171a4539ee56e0495",
+    "url": "/js/chunk-2d0c1282.32ef39aa.js.map"
+  },
+  {
+    "revision": "b0d419aef84c2f03eb8b",
+    "url": "/js/chunk-2d0c4d4f.caf2e407.js"
+  },
+  {
+    "revision": "b0d419aef84c2f03eb8b",
+    "url": "/js/chunk-2d0c4d4f.caf2e407.js.map"
+  },
+  {
+    "revision": "fac345c434d04d33159a",
+    "url": "/js/chunk-2d0cf888.49854748.js"
+  },
+  {
+    "revision": "fac345c434d04d33159a",
+    "url": "/js/chunk-2d0cf888.49854748.js.map"
+  },
+  {
+    "revision": "7065402293c4996a0e07",
+    "url": "/js/chunk-2d0d43d6.95fdf537.js"
+  },
+  {
+    "revision": "7065402293c4996a0e07",
+    "url": "/js/chunk-2d0d43d6.95fdf537.js.map"
+  },
+  {
+    "revision": "2e35419f1fedc443ffbe",
+    "url": "/js/chunk-2d0d6d35.6b6f6fc1.js"
+  },
+  {
+    "revision": "2e35419f1fedc443ffbe",
+    "url": "/js/chunk-2d0d6d35.6b6f6fc1.js.map"
+  },
+  {
+    "revision": "37fe4d2dbadfb8c3b38d",
+    "url": "/js/chunk-41948662.44e1bcd0.js"
+  },
+  {
+    "revision": "37fe4d2dbadfb8c3b38d",
+    "url": "/js/chunk-41948662.44e1bcd0.js.map"
+  },
+  {
+    "revision": "4304e5a4e1cc6c66ce90",
+    "url": "/js/chunk-78e97d9d.0dc31ede.js"
+  },
+  {
+    "revision": "4304e5a4e1cc6c66ce90",
+    "url": "/js/chunk-78e97d9d.0dc31ede.js.map"
+  },
+  {
+    "revision": "4464a44068c58ca149ce",
+    "url": "/js/chunk-7c0d28db.ef109ce3.js"
+  },
+  {
+    "revision": "4464a44068c58ca149ce",
+    "url": "/js/chunk-7c0d28db.ef109ce3.js.map"
+  },
+  {
+    "revision": "a62e1ce5258b1b6de58d",
+    "url": "/js/chunk-81b3ae8c.30f98dad.js"
+  },
+  {
+    "revision": "a62e1ce5258b1b6de58d",
+    "url": "/js/chunk-81b3ae8c.30f98dad.js.map"
+  },
+  {
+    "revision": "865981f8dcdb9d2f47ee",
+    "url": "/js/chunk-c425edd8.13b331c4.js"
+  },
+  {
+    "revision": "865981f8dcdb9d2f47ee",
+    "url": "/js/chunk-c425edd8.13b331c4.js.map"
+  },
+  {
+    "revision": "bef6038c42e90a8b8ade",
+    "url": "/js/chunk-e30132c2.53174d08.js"
+  },
+  {
+    "revision": "bef6038c42e90a8b8ade",
+    "url": "/js/chunk-e30132c2.53174d08.js.map"
+  },
+  {
+    "revision": "39e8a8842b70c28601f8",
+    "url": "/js/chunk-ecba9ee6.18303e47.js"
+  },
+  {
+    "revision": "39e8a8842b70c28601f8",
+    "url": "/js/chunk-ecba9ee6.18303e47.js.map"
+  },
+  {
+    "revision": "82c8ed2914602e218fdb",
+    "url": "/js/chunk-vendors.469a051d.js"
+  },
+  {
+    "revision": "82c8ed2914602e218fdb",
+    "url": "/js/chunk-vendors.469a051d.js.map"
+  },
+  {
+    "revision": "5c88a4e292af90104c3b1fa5319cbb61",
+    "url": "/js/kakao.min.js"
+  },
+  {
+    "revision": "d602e8168a4d832cf60b70d09bc737f9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "73fa3bde545642f73aedc9b4e1ca3589",
+    "url": "/tmp/left-banner.jpg"
+  },
+  {
+    "revision": "18fa8df8022e902065866f8345a46ca0",
+    "url": "/tmp/philgo-logo.jpg"
+  },
+  {
+    "revision": "39393735608245e0f2463af28f38fa0b",
+    "url": "/tmp/right-banner.jpg"
+  }
+]);
