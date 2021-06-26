aa07272a825e0a575c4a2ef23f898c05eb7bf492
JaeHo Song <thruthesky@gmail.com>
2021-06-26 16:52:38 +0900

    update wc_coutries

--- etc/install/sql/wc_countries.sql
+++ etc/install/sql/wc_countries.sql
@@ -17,9 +17,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- 데이터베이스: `centerx`
---
 
 -- --------------------------------------------------------
 
