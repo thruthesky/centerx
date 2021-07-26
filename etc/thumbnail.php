59b416063b351667639d7a458ffe98ecd70f997c
Jaeho Song <thruthesky@gmail.com>
2021-07-26 05:37:19 +0000

    update

--- etc/thumbnail.php
+++ etc/thumbnail.php
@@ -6,13 +6,12 @@ $path = zoomThumbnail($file->path, in('w', 200), in('h', 200), in('q', 95));
 header('Pragma: public');
 header('Cache-Control: max-age=86400');
 header('Expires: '. gmdate('D, d M Y H:i:s \G\M\T', time() + 86400));
-if ( THUMBNAIL_FORMAT == 'web' ) {
+if ( THUMBNAIL_FORMAT == 'webp' ) {
     header('Content-Type: image/webp');
 }
 else {
     header('Content-Type: image/jpeg');
 }
-
 
 
 $filename = basename($path);
@@ -27,8 +26,6 @@ switch( $file_extension ) {
     default:
         $ctype="image/jpeg"; // default to jpeg. 때로는 썸네일이 존재하는데, file extension 이 올바르지 않은 경우가 있다.
 }
-
-header('Content-type: ' . $ctype);
 
 readfile($path);
 
