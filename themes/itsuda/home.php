6a729f941dd7b5d90ef82dfdae22b261dfc14d25
jimingoal <jimingoal@gmail.com>
2022-04-04 22:23:31 +0900

    update homepage image

--- themes/itsuda/home.php
+++ themes/itsuda/home.php
@@ -2,10 +2,9 @@
 
 
 ?>
-<div class="mb-3 d-flex justify-content-end">
-    <a href="https://play.google.com/store/apps/details?id=com.itsuda50.app3" target="_blank"><img style="width: 240px;" src="themes/itsuda/img/android-download.png" hspace=10></a>
-    <a href="https://apps.apple.com/pk/app/%EC%9E%88%EC%88%98%EB%8B%A4/id1560827977" target="_blank"><img style="width: 240px;" src="themes/itsuda/img/ios-download.png"></a>
-</div>
+<!--<div class="mb-3 d-flex justify-content-end">-->
+
+<!--</div>-->
 
 <img id="home1" class="w-100" src="themes/itsuda/img/home1.jpg">
 <img id="home1" class="w-100" src="themes/itsuda/img/home2.jpg">
