0d3b6f90c967bd9fde231c99134f33902aaef1a1
jimingoal <jimingoal@gmail.com>
2022-04-04 22:24:33 +0900

    update homepage image

--- themes/itsuda/index.php
+++ themes/itsuda/index.php
@@ -78,7 +78,7 @@
             <div class="d-flex justify-content-between">
                 <div>
                     <div class="d-block w-200px">
-                        <a href="/"><img float="left" class="py-3 pl-5 w-100" src="themes/itsuda/img/logo.jpg"></a>
+                        <a href="/"><img align="left" class="py-3 pl-5 w-100" src="themes/itsuda/img/logo.jpg"></a>
                     </div>
                 </div>
                 <div class="mt-4">
@@ -86,8 +86,8 @@
                     <a class="p-2 scroll" style="font-size: 1rem" href="/#home3">서비스소개</a>
                     <a class="p-2 scroll" style="font-size: 1rem" href="/#home5">제휴서비스</a>
                     <a class="p-2" style="font-size: 1rem" href="/?doc.privacy">개인정보처리방침</a>
-                    <a href="https://play.google.com/store/apps/details?id=com.itsuda50.app3" target="_blank"><img  src="themes/itsuda/img/android-download.png"></a>
-                    <a href="https://apps.apple.com/pk/app/%EC%9E%88%EC%88%98%EB%8B%A4/id1560827977" target="_blank"><img " src="themes/itsuda/img/ios-download.png"></a>
+                    <a href="https://play.google.com/store/apps/details?id=com.itsuda50.app3" target="_blank"><img src="themes/itsuda/img/android-download.png"></a>
+                    <a href="https://apps.apple.com/pk/app/%EC%9E%88%EC%88%98%EB%8B%A4/id1560827977" target="_blank"><img src="themes/itsuda/img/ios-download.png"></a>
                     <?php if ( loggedIn() ) { ?>
                         <?php if ( admin() ) { ?>
                             <a class="p-2" style="font-size: 1.1rem" href="/?admin.index"><?=login()->name ?? 'Admin'?>(<?=login()->idx?>)</a>
